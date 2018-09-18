a525014fae1e683377cec9adff1a09e20e13b49d
priyanka.harkare@gmail.com <priyanka.harkare@gmail.com>
2018-09-18 13:44:11 -0400

    Add values to the buttons create variable called `credit` to show the sum of clicked buttons

--- src/app/insert-coin/insert-coin.component.ts
+++ src/app/insert-coin/insert-coin.component.ts
@@ -8,13 +8,20 @@ import { BalanceService } from '../balance/balance.service';
   providers: []
 })
 export class InsertCoinComponent implements OnInit {
+  credit : number;
   coinBalance = 0;
   constructor(public balanceService: BalanceService) { }
+  
 
   ngOnInit() {
     this.balanceService.onBalanceUpdated((balance) => {
       this.coinBalance = balance;
     });
   }
-
+    onClick= function(amount){
+    // console.log(this.coinBalance = 5)
+    // console.log(this.coinBalance)
+     this.credit = this.coinBalance + amount
+     console.log(`credit is`, this.credit)
+    }
 }
